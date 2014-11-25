f4bd0c3f83dddc3096f22bb07be600791b85e49b
Vincent Riemer <vincentriemer@gmail.com>
2014-11-25 13:50:01 -0500

    misc changes

--- src/scripts/Animator/animator.js
+++ src/scripts/Animator/animator.js
@@ -1,14 +1,15 @@
 /* @flow */
 
-var Animation = require("./animation");
-var EasingFactory = require("./ease");
+// imports
+var Animation     = require("./animation"),
+    EasingFactory = require("./ease");
 
 type Options = {
 
 };
 
 /**
- * The main animation api.
+ * The main animation API.
  * @constructor
  * @param {Options} options - the options which configure the animation library.
  */
@@ -17,7 +18,24 @@ var Animator = function(options?: Options) {
     this.options = options || {};
     this.elementMap = new Map();
 
-    this.addAnimation = function() {};
+    /**
+     * Add a new animation to the target and start it immediately
+     * @param {HTMLElement} target - the target HTML DOM element that will be animated                   
+     * @param {string} attribute - the attribute that will be animated
+     * @param {number} start - the attribute's starting value                       
+     * @param {number} end - the attribute's ending value                                          
+     * @param {number} duration - the number of frames the animation will take to complete
+     * @param {Function} ease - the easing function which is applied to the animation                                    
+     */
+    this.addAnimation = function(
+        target    : HTMLElement, 
+        attribute : string, 
+        start     : number, 
+        end       : number, 
+        duration  : number, 
+        ease      : Function) {
+
+    };
 };
 
 module.exports = Animator;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,10 @@
 /* @flow */
 
-var Animator = require("./Animator/animator");
-var Animation = require("./Animator/animation");
+var Animator = require("./Animator/animator"),
+  Animation = require("./Animator/animation"),
+  EasingFactory = require("./Animator/ease");
+
+var target = document.getElementById('target'),
+  animator = new Animator();
+
+animator.addAnimation(target, 'translateX', 0, 100, 100, EasingFactory.linear());  
