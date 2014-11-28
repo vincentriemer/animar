aa8b4d20c2e32784668ad3f23b322a1d802fe1e8
Vincent Riemer <vincentriemer@gmail.com>
2014-11-28 16:43:18 -0500

    added transform helper methods

--- src/scripts/Animator/helper.js
+++ src/scripts/Animator/helper.js
@@ -14,6 +14,50 @@ var Helper = {
       element.style.msTransform = transformString;
       element.style.OTransform = transformString;
       element.style.transform = transformString;
+    },
+
+    getTransformMatrix: function(element : HTMLElement) : Array<number> {
+      var computedStyle = window.getComputedStyle(element, null);
+      var transformString = computedStyle.getPropertyValue("-webkit-transform") ||
+                            computedStyle.getPropertyValue("-moz-transform") ||
+                            computedStyle.getPropertyValue("-ms-transform") ||
+                            computedStyle.getPropertyValue("-o-transform") ||
+                            computedStyle.getPropertyValue("transform") ||
+                            "matrix(1, 0, 0, 1, 0, 0)"; // assume that the element just hasn't had a transform applied to it yet
+
+      var values = transformString.split('(')[1].split(')')[0].split(',');
+      values = values.map(function(x){return parseInt(x)});
+      return values;
+    },
+
+    getTranslateX: function(element : HTMLElement) : number {
+      var values = this.getTransformMatrix(element);
+      return values[4];
+    },
+
+    getTranslateY: function(element : HTMLElement) : number {
+      var values = this.getTransformMatrix(element);
+      return values[5];
+    },
+
+    getScaleX: function(element : HTMLElement) : number {
+      var values = this.getTransformMatrix(element);
+      return values[0];
+    },
+
+    getScaleY: function(element : HTMLElement) : number {
+      var values = this.getTransformMatrix(element);
+      return values[3];
+    },
+
+    getRotation: function(element : HTMLElement) : number {
+      var values = this.getTransformMatrix(element);
+      var a = values[0],
+          b = values[1],
+          c = values[2],
+          d = values[3];
+
+      return Math.round(Math.atan2(b, a) * (180/Math.PI));
     }
 };
 
