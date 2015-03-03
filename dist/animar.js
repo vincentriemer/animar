c3b2c11be1d180e6f5fcdd609ed03f55499f5d25
Vincent Riemer <vincentriemer@gmail.com>
2015-03-03 12:46:43 -0500

    removed es6-collection polyfill dependancy and updated readme.

--- dist/animar.js
+++ dist/animar.js
@@ -48,79 +48,9 @@
 	    Helper = __webpack_require__(2),
 	    Constants = __webpack_require__(3);
 
-	__webpack_require__(4);  // TODO: Remove dependancy
-
 	var Animar = function() {
 	    this.elementMap = new Map();
 	    this.ticking = false;
-	};
-
-	Animar.prototype._createAttribute = function(attributeMap, animation) {
-	  var newAttributeObject = {
-	    model: animation.start,
-	    animations: []
-	  };
-	  attributeMap.set(animation.attribute, newAttributeObject);
-	  return attributeMap;
-	};
-
-	Animar.prototype._addAnimationToElement = function(attributeMap, args) {
-	  if (!attributeMap.has(args.attribute)) {
-	    attributeMap = this._createAttribute(attributeMap,args);
-	  }
-	  var currentAttribute = attributeMap.get(args.attribute),
-	    startValue  = currentAttribute.model - args.destination,
-	    changeInValue = 0 - startValue;
-
-	  currentAttribute.model = args.destination;
-
-	  var currentAnimation = {
-	    currentIteration: 0 - args.delay,
-	    startValue: startValue,
-	    changeInValue: changeInValue,
-	    totalIterations: args.duration,
-	    easingFunction: args.ease,
-	    loop: args.loop,
-	    delay: args.delay,
-	    wait: args.wait
-	  };
-	  currentAttribute.animations.push(currentAnimation);
-	  return currentAnimation;
-	};
-
-	Animar.prototype._addAnimationToMap = function(args) {
-	  if (!this.elementMap.has(args.element)) {
-	    this.elementMap.set(args.element, new Map());
-	  }
-	  return this._addAnimationToElement(this.elementMap.get(args.element),args);
-	};
-
-	Animar.prototype._addAnimation = function(element, attribute, destination, options) {
-	  options = options || {};
-
-	  // DEFAULTS & PARAMETER TRANSFORMATION
-	  options.easing = options.easing || 'linear';
-	  options.easing = typeof options.easing === 'string' ? EasingFactory[options.easing]() : options.easing;
-	  options.duration = options.duration || 60;
-	  options.loop = options.loop || false;
-	  options.delay = options.delay || 0;
-	  options.start = options.start === undefined ? Helper.getStartValue([element, attribute]) : options.start;
-	  options.wait = options.wait || 0;
-
-	  var newAnimation = {
-	    element: element,
-	    attribute: attribute,
-	    start: options.start,
-	    destination: destination,
-	    duration: options.duration,
-	    ease: options.easing,
-	    delay: options.delay,
-	    loop: options.loop,
-	    wait: options.wait
-	  };
-	  var currentAnimation = this._addAnimationToMap(newAnimation);
-	  this._requestTick();
-	  return currentAnimation;
 	};
 
 	Animar.prototype.add = function(element, attributes, options) {
@@ -198,6 +128,89 @@
 	  };
 	};
 
+	Animar.prototype._addAnimation = function(element, attribute, destination, options) {
+	  options = options || {};
+
+	  // DEFAULTS & PARAMETER TRANSFORMATION
+	  options.easing = options.easing || 'linear';
+	  options.easing = typeof options.easing === 'string' ? EasingFactory[options.easing]() : options.easing;
+	  options.duration = options.duration || 60;
+	  options.loop = options.loop || false;
+	  options.delay = options.delay || 0;
+	  options.start = options.start === undefined ? Helper.getStartValue([element, attribute]) : options.start;
+	  options.wait = options.wait || 0;
+
+	  var newAnimation = {
+	    element: element,
+	    attribute: attribute,
+	    start: options.start,
+	    destination: destination,
+	    duration: options.duration,
+	    ease: options.easing,
+	    delay: options.delay,
+	    loop: options.loop,
+	    wait: options.wait
+	  };
+	  var currentAnimation = this._addAnimationToMap(newAnimation);
+	  this._requestTick();
+	  return currentAnimation;
+	};
+
+	Animar.prototype._addAnimationToMap = function(args) {
+	  if (!this.elementMap.has(args.element)) {
+	    this.elementMap.set(args.element, new Map());
+	  }
+	  return this._addAnimationToElement(this.elementMap.get(args.element),args);
+	};
+
+	Animar.prototype._addAnimationToElement = function(attributeMap, args) {
+	  if (!attributeMap.has(args.attribute)) {
+	    attributeMap = this._createAttribute(attributeMap,args);
+	  }
+	  var currentAttribute = attributeMap.get(args.attribute),
+	    startValue  = currentAttribute.model - args.destination,
+	    changeInValue = 0 - startValue;
+
+	  currentAttribute.model = args.destination;
+
+	  var currentAnimation = {
+	    currentIteration: 0 - args.delay,
+	    startValue: startValue,
+	    changeInValue: changeInValue,
+	    totalIterations: args.duration,
+	    easingFunction: args.ease,
+	    loop: args.loop,
+	    delay: args.delay,
+	    wait: args.wait
+	  };
+	  currentAttribute.animations.push(currentAnimation);
+	  return currentAnimation;
+	};
+
+	Animar.prototype._createAttribute = function(attributeMap, animation) {
+	  var newAttributeObject = {
+	    model: animation.start,
+	    animations: []
+	  };
+	  attributeMap.set(animation.attribute, newAttributeObject);
+	  return attributeMap;
+	};
+
+	Animar.prototype._update = function() {
+	  var animationsRemaining = this._renderDOM();
+	  this._stepFrame();
+	  this.ticking = false;
+	  if (animationsRemaining) {
+	    this._requestTick();
+	  }
+	};
+
+	Animar.prototype._requestTick = function() {
+	  if (!this.ticking) {
+	    window.requestAnimationFrame(this._update.bind(this));
+	    this.ticking = true;
+	  }
+	};
 
 	Animar.prototype._calculateAnimationValue = function(animations) {
 	  var result = 0;
@@ -275,22 +288,6 @@
 	      value.animations = updatedAnimations;
 	    });
 	  });
-	};
-
-	Animar.prototype._update = function() {
-	  var animationsRemaining = this._renderDOM();
-	  this._stepFrame();
-	  this.ticking = false;
-	  if (animationsRemaining) { 
-	    this._requestTick();
-	  }
-	};
-
-	Animar.prototype._requestTick = function() {
-	  if (!this.ticking) {
-	    window.requestAnimationFrame(this._update.bind(this));
-	    this.ticking = true;
-	  }
 	};
 
 	module.exports = Animar;
@@ -546,241 +543,6 @@
 	};
 
 	module.exports = Constants;
-
-/***/ },
-/* 4 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/* WEBPACK VAR INJECTION */(function(global) {(function (exports) {'use strict';
-	  //shared pointer
-	  var i;
-	  //shortcuts
-	  var defineProperty = Object.defineProperty, is = function(a,b) { return isNaN(a)? isNaN(b): a === b; };
-
-
-	  //Polyfill global objects
-	  if (typeof WeakMap == 'undefined') {
-	    exports.WeakMap = createCollection({
-	      // WeakMap#delete(key:void*):boolean
-	      'delete': sharedDelete,
-	      // WeakMap#clear():
-	      clear: sharedClear,
-	      // WeakMap#get(key:void*):void*
-	      get: sharedGet,
-	      // WeakMap#has(key:void*):boolean
-	      has: mapHas,
-	      // WeakMap#set(key:void*, value:void*):void
-	      set: sharedSet
-	    }, true);
-	  }
-
-	  if (typeof Map == 'undefined') {
-	    exports.Map = createCollection({
-	      // WeakMap#delete(key:void*):boolean
-	      'delete': sharedDelete,
-	      //:was Map#get(key:void*[, d3fault:void*]):void*
-	      // Map#has(key:void*):boolean
-	      has: mapHas,
-	      // Map#get(key:void*):boolean
-	      get: sharedGet,
-	      // Map#set(key:void*, value:void*):void
-	      set: sharedSet,
-	      // Map#keys(void):Iterator
-	      keys: sharedKeys,
-	      // Map#values(void):Iterator
-	      values: sharedValues,
-	      // Map#entries(void):Iterator
-	      entries: mapEntries,
-	      // Map#forEach(callback:Function, context:void*):void ==> callback.call(context, key, value, mapObject) === not in specs`
-	      forEach: sharedForEach,
-	      // Map#clear():
-	      clear: sharedClear
-	    });
-	  }
-
-	  if (typeof Set == 'undefined') {
-	    exports.Set = createCollection({
-	      // Set#has(value:void*):boolean
-	      has: setHas,
-	      // Set#add(value:void*):boolean
-	      add: sharedAdd,
-	      // Set#delete(key:void*):boolean
-	      'delete': sharedDelete,
-	      // Set#clear():
-	      clear: sharedClear,
-	      // Set#keys(void):Iterator
-	      keys: sharedValues, // specs actually say "the same function object as the initial value of the values property"
-	      // Set#values(void):Iterator
-	      values: sharedValues,
-	      // Set#entries(void):Iterator
-	      entries: setEntries,
-	      // Set#forEach(callback:Function, context:void*):void ==> callback.call(context, value, index) === not in specs
-	      forEach: sharedSetIterate
-	    });
-	  }
-
-	  if (typeof WeakSet == 'undefined') {
-	    exports.WeakSet = createCollection({
-	      // WeakSet#delete(key:void*):boolean
-	      'delete': sharedDelete,
-	      // WeakSet#add(value:void*):boolean
-	      add: sharedAdd,
-	      // WeakSet#clear():
-	      clear: sharedClear,
-	      // WeakSet#has(value:void*):boolean
-	      has: setHas
-	    }, true);
-	  }
-
-
-	  /**
-	   * ES6 collection constructor
-	   * @return {Function} a collection class
-	   */
-	  function createCollection(proto, objectOnly){
-	    function Collection(a){
-	      if (!this || this.constructor !== Collection) return new Collection(a);
-	      this._keys = [];
-	      this._values = [];
-	      this.objectOnly = objectOnly;
-
-	      //parse initial iterable argument passed
-	      if (a) init.call(this, a);
-	    }
-
-	    //define size for non object-only collections
-	    if (!objectOnly) {
-	      defineProperty(proto, 'size', {
-	        get: sharedSize
-	      });
-	    }
-
-	    //set prototype
-	    proto.constructor = Collection;
-	    Collection.prototype = proto;
-
-	    return Collection;
-	  }
-
-
-	  /** parse initial iterable argument passed */
-	  function init(a){
-	    var i;
-	    //init Set argument, like `[1,2,3,{}]`
-	    if (this.add)
-	      a.forEach(this.add, this);
-	    //init Map argument like `[[1,2], [{}, 4]]`
-	    else
-	      a.forEach(function(a){this.set(a[0],a[1])}, this);
-	  }
-
-
-	  /** delete */
-	  function sharedDelete(key) {
-	    if (this.has(key)) {
-	      this._keys.splice(i, 1);
-	      this._values.splice(i, 1);
-	    }
-	    // Aurora here does it while Canary doesn't
-	    return -1 < i;
-	  };
-
-	  function sharedGet(key) {
-	    return this.has(key) ? this._values[i] : undefined;
-	  }
-
-	  function has(list, key) {
-	    if (this.objectOnly && key !== Object(key))
-	      throw new TypeError("Invalid value used as weak collection key");
-	    //NaN or 0 passed
-	    if (key != key || key === 0) for (i = list.length; i-- && !is(list[i], key);){}
-	    else i = list.indexOf(key);
-	    return -1 < i;
-	  }
-
-	  function setHas(value) {
-	    return has.call(this, this._values, value);
-	  }
-
-	  function mapHas(value) {
-	    return has.call(this, this._keys, value);
-	  }
-
-	  /** @chainable */
-	  function sharedSet(key, value) {
-	    this.has(key) ?
-	      this._values[i] = value
-	      :
-	      this._values[this._keys.push(key) - 1] = value
-	    ;
-	    return this;
-	  }
-
-	  /** @chainable */
-	  function sharedAdd(value) {
-	    if (!this.has(value)) this._values.push(value);
-	    return this;
-	  }
-
-	  function sharedClear() {
-	    this._values.length = 0;
-	  }
-
-	  /** keys, values, and iterate related methods */
-	  function sharedKeys() {
-	    return sharedIterator(this._keys);
-	  }
-
-	  function sharedValues() {
-	    return sharedIterator(this._values);
-	  }
-
-	  function mapEntries() {
-	    return sharedIterator(this._keys, this._values);
-	  }
-
-	  function setEntries() {
-	    return sharedIterator(this._values, this._values);
-	  }
-
-	  function sharedIterator(array, array2) {
-	    var j = 0, done = false;
-	    return {
-	      next: function() {
-	        var v;
-	        if (!done && j < array.length) {
-	          v = array2 ? [array[j], array2[j]]: array[j];
-	          j += 1;
-	        } else {
-	          done = true;
-	        }
-	        return { done: done, value: v };
-	      }
-	    };
-	  }
-
-	  function sharedSize() {
-	    return this._values.length;
-	  }
-
-	  function sharedForEach(callback, context) {
-	    var self = this;
-	    var values = self._values.slice();
-	    self._keys.slice().forEach(function(key, n){
-	      callback.call(context, values[n], key, self);
-	    });
-	  }
-
-	  function sharedSetIterate(callback, context) {
-	    var self = this;
-	    self._values.slice().forEach(function(value){
-	      callback.call(context, value, value, self);
-	    });
-	  }
-
-	})(typeof exports != 'undefined' && typeof global != 'undefined' ? global : window );
-	
-	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ }
 /******/ ])
