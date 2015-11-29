4c4dc57730dc5b7ba54c00da488864435615c054
Vincent Riemer <vincentriemer@gmail.com>
2015-11-28 22:50:25 -0500

    added wallaby.js configuration file

---
+++ wallaby.js
@@ -0,0 +1,27 @@
+var babel = require('babel-core');
+
+var babelConfig = JSON.parse(require('fs').readFileSync(require('path').join(__dirname, '.babelrc')));
+babelConfig.babel = babel;
+
+module.exports = function (wallaby) {
+    return {
+      files: [
+        'src/*.js'
+      ],
+
+      tests: [
+        'test/*.spec.js'
+      ],
+
+      testFramework: 'mocha',
+
+      env: {
+        type: 'node',
+        runner: 'node'
+      },
+
+      compilers: {
+        '**/*.js': wallaby.compilers.babel(babelConfig)
+      }
+    };
+  };
