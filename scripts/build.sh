53a326f2b2f901380c68b07f6f8c0160aa64bb6f
Vincent Riemer <vincentriemer@gmail.com>
2015-12-21 12:54:14 -0500

    removed versioned builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 webpack --config webpack/config.js
-webpack --config webpack/config.versioned.js
 webpack --config webpack/config.minified.js
-webpack --config webpack/config.versioned.minified.js
